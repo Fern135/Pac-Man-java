2944e7cafd17b318270249f9fb493daee2beb04a
fernando camblor <dante.camblor@gmail.com>
2018-10-04 09:36:38 -0400

    Slow Progress

--- Pac-Man/src/pac/man/PacMan.java
+++ Pac-Man/src/pac/man/PacMan.java
@@ -11,6 +11,8 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 /**
@@ -21,24 +23,50 @@ public class PacMan extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
         
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+        // setting the physical buttons                 //
+        Button up = new Button();//  up                 //
+        Button left = new Button();// left              //
+        Button down = new Button();// down              //
+        Button right = new Button();// right            //
+        /************************************************/
+        
+        // the background                                      //
+        Rectangle backGround = new Rectangle(10000, 10000);    //
+        /******************************************************/
+        
+        // setting the text for the physical buttons on the screen  //
+        up.setText("^");                                            //          
+        left.setText("<");                                          //
+        down.setText("V");                                          //
+        right.setText(">");                                         //
+        /************************************************************/
+        
+        
+        // setting the background color for the background  //
+        backGround.setFill(Color.BLACK);                    //
+        /****************************************************/
+        
+        up.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
                 System.out.println("Hello World!");
             }
              });
         
+        // initializing what'll be the main window frame
         StackPane root = new StackPane();
         
-        // addibg materials to the screen
-        root.getChildren().add(btn);
+        root.getChildren().add(backGround);
+        // adding materials to the screen
+        // root.getChildren().add(up);
+        // root.getChildren().add(left);
+        root.getChildren().add(down);
+        
         
         
         // setting the size of the window, what'll be included and showing it
-        Scene scene = new Scene(root, 300, 250);
+        Scene scene = new Scene(root, 600, 600);
         
         primaryStage.setTitle("Pac-man Java");
         primaryStage.setScene(scene);
